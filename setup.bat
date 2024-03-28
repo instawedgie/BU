49cb3278a8001a23ad1659a7d415d4ca0dbc361e
instawedgie <155210311+instawedgie@users.noreply.github.com>
2024-03-28 19:42:45 -0400

    Push pip output into setup.logs, use setup.txt to mark setup

--- setup.bat
+++ setup.bat
@@ -1,19 +1,21 @@
 @echo off 
 
+echo "--- Start Setup ---" > setup.logs
+
 echo - Updating pip... 
-python -m ensurepip 
+python -m ensurepip >> setup.logs 2>&1
 
 echo - Installing virtualenv...
-python -m pip install virtualenv
+python -m pip install virtualenv >> setup.logs 2>&1
 
 echo - Setting up venv... 
-python -m virtualenv venv 
+python -m virtualenv venv >> setup.logs 2>&1
 
 echo - Activating venv...
-call .\venv\Scripts\activate 
+call .\venv\Scripts\activate >> setup.logs 2>&1
 
 echo - Installing requirements... 
-python -m ensurepip 
-python -m pip install -r requirements.txt 
+python -m ensurepip >> setup.logs 2>&1
+python -m pip install -r requirements.txt >> setup.logs 2>&1
 
-echo 'Successful setup' > setup.logs 
+echo 'Game Fully Setup' > setup.txt
